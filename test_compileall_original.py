12c5a1b77fa81ee720cb92b77d7cf0041febe07e
Lumir Balhar <lbalhar@redhat.com>
2019-04-01 14:06:28 +0200

    Add tests for new features

--- test_compileall_original.py
+++ test_compileall_original.py
@@ -275,6 +275,61 @@ class CompileallTestsBase:
                                maxlevels=110, quiet=True)
         self.assertTrue(os.path.isfile(self.bc_path_long))
 
+    def test_strip_only(self):
+        fullpath = ["test", "build", "real", "path"]
+        path = os.path.join(self.directory, *fullpath)
+        os.makedirs(path)
+        script = script_helper.make_script(path, "test", "1 / 0")
+        bc = importlib.util.cache_from_source(script)
+        stripdir = os.path.join(self.directory, *fullpath[:2])
+        compileall.compile_dir(path, quiet=True, stripdir=stripdir)
+        rc, out, err = script_helper.assert_python_failure(bc)
+        expected_in = os.path.join(*fullpath[2:])
+        self.assertIn(
+            expected_in,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+        self.assertNotIn(
+            stripdir,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+
+    def test_append_only(self):
+        fullpath = ["test", "build", "real", "path"]
+        path = os.path.join(self.directory, *fullpath)
+        os.makedirs(path)
+        script = script_helper.make_script(path, "test", "1 / 0")
+        bc = importlib.util.cache_from_source(script)
+        appenddir = "/foo"
+        compileall.compile_dir(path, quiet=True, appenddir=appenddir)
+        rc, out, err = script_helper.assert_python_failure(bc)
+        expected_in = os.path.join(appenddir, self.directory, *fullpath)
+        self.assertIn(
+            expected_in,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+
+    def test_strip_and_append(self):
+        fullpath = ["test", "build", "real", "path"]
+        path = os.path.join(self.directory, *fullpath)
+        os.makedirs(path)
+        script = script_helper.make_script(path, "test", "1 / 0")
+        bc = importlib.util.cache_from_source(script)
+        stripdir = os.path.join(self.directory, *fullpath[:2])
+        appenddir = "/foo"
+        compileall.compile_dir(path, quiet=True,
+                               stripdir=stripdir, appenddir=appenddir)
+        rc, out, err = script_helper.assert_python_failure(bc)
+        expected_in = os.path.join(appenddir, *fullpath[2:])
+        self.assertIn(
+            expected_in,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+        self.assertNotIn(
+            stripdir,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+
 
 class CompileallTestsWithSourceEpoch(CompileallTestsBase,
                                      unittest.TestCase,
@@ -698,6 +753,28 @@ class CommandLineTestsBase:
             compileall.main()
             self.assertTrue(compile_dir.called)
             self.assertEqual(compile_dir.call_args[-1]['workers'], None)
+
+    def test_strip_and_append(self):
+        fullpath = ["test", "build", "real", "path"]
+        path = os.path.join(self.directory, *fullpath)
+        os.makedirs(path)
+        script = script_helper.make_script(path, "test", "1 / 0")
+        bc = importlib.util.cache_from_source(script)
+        stripdir = os.path.join(self.directory, *fullpath[:2])
+        appenddir = "/foo"
+        compileall.compile_dir(path, quiet=True,
+                               stripdir=stripdir, appenddir=appenddir)
+        self.assertRunOK("-s", stripdir, "-a", appenddir, path)
+        rc, out, err = script_helper.assert_python_failure(bc)
+        expected_in = os.path.join(appenddir, *fullpath[2:])
+        self.assertIn(
+            expected_in,
+            str(err, encoding=sys.getdefaultencoding())
+        )
+        self.assertNotIn(
+            stripdir,
+            str(err, encoding=sys.getdefaultencoding())
+        )
 
 
 class CommmandLineTestsWithSourceEpoch(CommandLineTestsBase,
